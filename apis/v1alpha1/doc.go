54f3c41af1f98eeef00a96feda32bbc666843ca9
fmendieta <fmendieta@eservicios.indra.es>
2025-08-05 16:16:16 +0200

    restart the ci

--- apis/v1alpha1/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Copyright 2021 Upbound Inc.
-*/
-
-// Package v1alpha1 contains the core resources of the Proxmox BPG jet provider.
-// +kubebuilder:object:generate=true
-// +groupName=proxmoxbpg.crossplane.io
-// +versionName=v1alpha1
-package v1alpha1
